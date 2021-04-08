9ce5dd379c140b657f3cd28ae9ae2a30c7ccdc44
Victorio G. Montenegro <82038809+vgmontenegro@users.noreply.github.com>
2021-04-08 13:22:01 -0300

    Mudando tema, título e estrutura do código

--- teste_git.py
+++ teste_git.py
@@ -1,11 +1,10 @@
-print("Testando o github - corrindo bug na master")
+print("Testando o github\n")
 
-print("Título")
-print("Carros legais!")
+#Título
+print("Carros legais!\n")
 
-print("Desenvolvimento")
-print("Conflitos são resolvidos facilmente. Deu certo!")
-
+# Desenvolvimento
+print("Esses carros são extremamente interessantes e incomparáveis entre sí\n")
 print("Lista de carros: ")
 print("Subaru WRX, Porsche 911 GT3 RS, Mitsubishi L200 Triton Outdoor GLX, "
       "Volvo XC60.")
