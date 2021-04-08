323fd2e136caa1fd2c90423ed0ff794ef8b75e31
Victorio G. Montenegro <82038809+vgmontenegro@users.noreply.github.com>
2021-04-08 13:05:21 -0300

    Adicionando lista de carros

--- teste_git.py
+++ teste_git.py
@@ -6,3 +6,7 @@ print("Gerando conflitos e corrigindo!")
 print("Desenvolvimento")
 print("Conflitos são resolvidos facilmente. Deu certo!")
 
+print("Lista de carros: ")
+print("WRX"
+      "911 GT3 RS"
+      "Triton L200 Outdoor")
