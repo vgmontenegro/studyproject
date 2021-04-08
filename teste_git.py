c50f3eda0f40a1e168df5613cd18ca63072ed694
Victorio G. Montenegro <82038809+vgmontenegro@users.noreply.github.com>
2021-04-08 13:17:33 -0300

    Adicionando novo carro a lista

--- teste_git.py
+++ teste_git.py
@@ -7,4 +7,5 @@ print("Desenvolvimento")
 print("Conflitos são resolvidos facilmente. Deu certo!")
 
 print("Lista de carros: ")
-print("Subaru WRX, Porsche 911 GT3 RS, Mitsubishi L200 Triton Outdoor GLX")
+print("Subaru WRX, Porsche 911 GT3 RS, Mitsubishi L200 Triton Outdoor GLX, "
+      "Volvo XC60.")
